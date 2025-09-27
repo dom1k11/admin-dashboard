a142ca829404b5c498905890a51f49f621146d54
Dominik Sedusov <dom1k11@outlook.com>
2025-09-27 09:38:28 +0300

    port fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,7 @@
 import app from './app';
 import 'dotenv/config';
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running at http://localhost:${PORT}`);
   const db_url = process.env.DATABASE_URL;
